2ed05ba7848be7b55e3852411ffc77b64dc66b1b
Your Name <you@example.com>
2026-02-09 19:02:51 +0300

    pen tool generated data edit bug issue resolved

--- src-js/views-editor/src/edit-tools-pen.js
+++ src-js/views-editor/src/edit-tools-pen.js
@@ -89,8 +89,9 @@ export class PenToolCubic extends BaseTool {
       return {};
     }
     const path = glyphController.instance.path;
+    const generatedContourIndices = this._getGeneratedContourIndexSet();
 
-    const appendInfo = getAppendInfo(path, this.sceneController.selection);
+    const appendInfo = getAppendInfo(path, this.sceneController.selection, generatedContourIndices);
     if (hoveredPointIndex === undefined && appendInfo.createContour) {
       const point = this.sceneController.localPoint(event);
       // The following max() call makes sure that the margin is never
@@ -99,6 +100,9 @@ export class PenToolCubic extends BaseTool {
       // with a max precision of 0.001.
       const size = Math.max(1, this.sceneController.mouseClickMargin);
       const hit = this.sceneModel.pathHitAtPoint(point, size);
+      if (this._isHitOnGeneratedContour(path, hit, generatedContourIndices)) {
+        return {};
+      }
       if (event.altKey && hit.segment?.points?.length === 2) {
         const pt1 = hit.segment.points[0];
         const pt2 = hit.segment.points[1];
@@ -118,6 +122,9 @@ export class PenToolCubic extends BaseTool {
     }
 
     if (hoveredPointIndex === undefined || appendInfo.createContour) {
+      return {};
+    }
+    if (this._isPointInGeneratedContour(path, hoveredPointIndex, generatedContourIndices)) {
       return {};
     }
 
@@ -147,6 +154,42 @@ export class PenToolCubic extends BaseTool {
     return { targetPoint: path.getPoint(hoveredPointIndex) };
   }
 
+  _getGeneratedContourIndexSet() {
+    const positionedGlyph = this.sceneModel.getSelectedPositionedGlyph();
+    if (!positionedGlyph?.varGlyph?.glyph?.layers) {
+      return new Set();
+    }
+    const editLayerName =
+      this.sceneModel.sceneSettings?.editLayerName || positionedGlyph.glyph?.layerName;
+    const layer = positionedGlyph.varGlyph.glyph.layers?.[editLayerName];
+    const indices = layer?.customData?.["fontra.skeleton"]?.generatedContourIndices || [];
+    return new Set(indices);
+  }
+
+  _isPointInGeneratedContour(path, pointIndex, generatedContourIndices) {
+    if (pointIndex === undefined || !generatedContourIndices?.size) {
+      return false;
+    }
+    const [contourIndex] = path.getContourAndPointIndex(pointIndex);
+    return generatedContourIndices.has(contourIndex);
+  }
+
+  _isHitOnGeneratedContour(path, hit, generatedContourIndices) {
+    if (!hit || !generatedContourIndices?.size) {
+      return false;
+    }
+    const parentPointIndices = hit.segment?.parentPointIndices;
+    if (parentPointIndices?.length) {
+      const [contourIndex] = path.getContourAndPointIndex(parentPointIndices[0]);
+      return generatedContourIndices.has(contourIndex);
+    }
+    if (hit.pointIndex !== undefined) {
+      const [contourIndex] = path.getContourAndPointIndex(hit.pointIndex);
+      return generatedContourIndices.has(contourIndex);
+    }
+    return false;
+  }
+
   async handleDrag(eventStream, initialEvent) {
     if (!this.sceneModel.selectedGlyph?.isEditing) {
       await this.editor.tools["pointer-tool"].handleDrag(eventStream, initialEvent);
@@ -164,6 +207,19 @@ export class PenToolCubic extends BaseTool {
   }
 
   async _handleInsertPoint() {
+    const positionedGlyph = this.sceneModel.getSelectedPositionedGlyph();
+    const path = positionedGlyph?.glyph?.path;
+    if (
+      path &&
+      this._isHitOnGeneratedContour(
+        path,
+        this.sceneModel.pathConnectTargetPoint,
+        this._getGeneratedContourIndexSet()
+      )
+    ) {
+      delete this.sceneModel.pathConnectTargetPoint;
+      return;
+    }
     await this.sceneController.editLayersAndRecordChanges((layerGlyphs) => {
       const selection = new Set();
       for (const layerGlyph of Object.values(layerGlyphs)) {
@@ -180,6 +236,19 @@ export class PenToolCubic extends BaseTool {
   }
 
   async _handleInsertHandles() {
+    const positionedGlyph = this.sceneModel.getSelectedPositionedGlyph();
+    const path = positionedGlyph?.glyph?.path;
+    if (
+      path &&
+      this._isHitOnGeneratedContour(
+        path,
+        this.sceneModel.pathInsertHandles?.hit,
+        this._getGeneratedContourIndexSet()
+      )
+    ) {
+      delete this.sceneModel.pathInsertHandles;
+      return;
+    }
     const segmentPointIndices =
       this.sceneModel.pathInsertHandles.hit.segment.pointIndices;
     await this.sceneController.editLayersAndRecordChanges((layerGlyphs) => {
@@ -287,8 +356,9 @@ export class PenToolQuad extends PenToolCubic {
       return {};
     }
     const path = glyphController.instance.path;
+    const generatedContourIndices = this._getGeneratedContourIndexSet();
 
-    const appendInfo = getAppendInfo(path, this.sceneController.selection);
+    const appendInfo = getAppendInfo(path, this.sceneController.selection, generatedContourIndices);
     if (hoveredPointIndex === undefined && appendInfo.createContour) {
       const point = this.sceneController.localPoint(event);
       // The following max() call makes sure that the margin is never
@@ -297,6 +367,9 @@ export class PenToolQuad extends PenToolCubic {
       // with a max precision of 0.001.
       const size = Math.max(1, this.sceneController.mouseClickMargin);
       const hit = this.sceneModel.pathHitAtPoint(point, size);
+      if (this._isHitOnGeneratedContour(path, hit, generatedContourIndices)) {
+        return {};
+      }
       if (event.altKey && hit.segment?.points?.length === 2) {
         const pt1 = hit.segment.points[0];
         const pt2 = hit.segment.points[1];
@@ -323,6 +396,9 @@ export class PenToolQuad extends PenToolCubic {
     }
 
     if (hoveredPointIndex === undefined || appendInfo.createContour) {
+      return {};
+    }
+    if (this._isPointInGeneratedContour(path, hoveredPointIndex, generatedContourIndices)) {
       return {};
     }
 
@@ -359,7 +435,11 @@ const AppendModes = {
 };
 
 function getPenToolBehavior(sceneController, initialEvent, path, curveType) {
-  const appendInfo = getAppendInfo(path, sceneController.selection);
+  const appendInfo = getAppendInfo(
+    path,
+    sceneController.selection,
+    getGeneratedContourIndexSet(sceneController)
+  );
 
   let behaviorFuncs;
 
@@ -720,13 +800,16 @@ function getPointSelectionAbs(pointIndex) {
   return new Set([`point/${pointIndex}`]);
 }
 
-function getAppendInfo(path, selection) {
+function getAppendInfo(path, selection, blockedContourIndices = undefined) {
   if (selection.size === 1) {
     const { point: pointSelection } = parseSelection(selection);
     const pointIndex = pointSelection?.[0];
     if (pointIndex !== undefined && pointIndex < path.numPoints) {
       const [contourIndex, contourPointIndex] =
         path.getContourAndPointIndex(pointIndex);
+      if (blockedContourIndices?.has(contourIndex)) {
+        return getCreateContourAppendInfo(path);
+      }
       const numPointsContour = path.getNumPointsOfContour(contourIndex);
       if (
         !path.contourInfo[contourIndex].isClosed &&
@@ -749,6 +832,10 @@ function getAppendInfo(path, selection) {
       }
     }
   }
+  return getCreateContourAppendInfo(path);
+}
+
+function getCreateContourAppendInfo(path) {
   return {
     contourIndex: path.contourInfo.length,
     contourPointIndex: 0,
@@ -756,6 +843,19 @@ function getAppendInfo(path, selection) {
     isOnCurve: undefined,
     createContour: true,
   };
+}
+
+function getGeneratedContourIndexSet(sceneController) {
+  const sceneModel = sceneController?.sceneModel;
+  const positionedGlyph = sceneModel?.getSelectedPositionedGlyph?.();
+  if (!positionedGlyph?.varGlyph?.glyph?.layers) {
+    return new Set();
+  }
+  const editLayerName =
+    sceneModel?.sceneSettings?.editLayerName || positionedGlyph.glyph?.layerName;
+  const layer = positionedGlyph.varGlyph.glyph.layers?.[editLayerName];
+  const indices = layer?.customData?.["fontra.skeleton"]?.generatedContourIndices || [];
+  return new Set(indices);
 }
 
 function emptyContour() {
