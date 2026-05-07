1954459ab26d226a657c67cd44f6f05908decd4e
Qwerasd <qwerasd205@users.noreply.github.com>
2026-05-07 16:21:57 -0400

    convert rectangle.js -> rectangle.ts

--- src-js/fontra-core/src/rectangle.ts
+++ src-js/fontra-core/src/rectangle.ts
@@ -1,16 +1,35 @@
 import { isNumber } from "./utils.js";
 
-export function pointInRect(x, y, rect) {
+export type Point = {
+  x: number;
+  y: number;
+};
+
+export type Rect = {
+  xMin: number;
+  yMin: number;
+  xMax: number;
+  yMax: number;
+};
+
+export type Size = {
+  width: number;
+  height: number;
+};
+
+export function pointInRect(x: number, y: number, rect: Rect) {
   if (!rect) {
     return false;
   }
   return x >= rect.xMin && x <= rect.xMax && y >= rect.yMin && y <= rect.yMax;
 }
 
-export function centeredRect(x, y, width, height) {
-  if (height === undefined) {
-    height = width;
-  }
+export function centeredRect(
+  x: number,
+  y: number,
+  width: number,
+  height: number = width
+) {
   const halfWidth = width / 2;
   const halfHeight = height / 2;
   return {
@@ -21,17 +40,16 @@ export function centeredRect(x, y, width, height) {
   };
 }
 
-export function normalizeRect(rect) {
-  const nRect = {
+export function normalizeRect(rect: Rect): Rect {
+  return {
     xMin: Math.min(rect.xMin, rect.xMax),
     yMin: Math.min(rect.yMin, rect.yMax),
     xMax: Math.max(rect.xMin, rect.xMax),
     yMax: Math.max(rect.yMin, rect.yMax),
   };
-  return nRect;
 }
 
-export function sectRect(rect1, rect2) {
+export function sectRect(rect1: Rect, rect2: Rect): Rect | undefined {
   // Test for rectangle-rectangle intersection.
 
   // Args:
@@ -52,7 +70,7 @@ export function sectRect(rect1, rect2) {
   return { xMin: xMin, yMin: yMin, xMax: xMax, yMax: yMax };
 }
 
-export function unionRect(...rectangles) {
+export function unionRect(...rectangles: Rect[]): Rect | undefined {
   if (!rectangles.length) {
     return undefined;
   }
@@ -71,7 +89,7 @@ export function unionRect(...rectangles) {
   return { xMin: xMin, yMin: yMin, xMax: xMax, yMax: yMax };
 }
 
-export function offsetRect(rect, x, y) {
+export function offsetRect(rect: Rect, x: number, y: number): Rect {
   return {
     xMin: rect.xMin + x,
     yMin: rect.yMin + y,
@@ -80,10 +98,7 @@ export function offsetRect(rect, x, y) {
   };
 }
 
-export function scaleRect(rect, sx, sy) {
-  if (sy === undefined) {
-    sy = sx;
-  }
+export function scaleRect(rect: Rect, sx: number, sy: number = sx): Rect {
   return {
     xMin: rect.xMin * sx,
     yMin: rect.yMin * sy,
@@ -92,7 +107,7 @@ export function scaleRect(rect, sx, sy) {
   };
 }
 
-export function insetRect(rect, dx, dy) {
+export function insetRect(rect: Rect, dx: number, dy: number): Rect {
   return {
     xMin: rect.xMin + dx,
     yMin: rect.yMin + dy,
@@ -101,7 +116,7 @@ export function insetRect(rect, dx, dy) {
   };
 }
 
-export function equalRect(rect1, rect2) {
+export function equalRect(rect1: Rect, rect2: Rect) {
   return (
     rect1.xMin === rect2.xMin &&
     rect1.yMin === rect2.yMin &&
@@ -110,34 +125,42 @@ export function equalRect(rect1, rect2) {
   );
 }
 
-export function rectCenter(rect) {
-  return { x: (rect.xMin + rect.xMax) / 2, y: (rect.yMin + rect.yMax) / 2 };
+export function rectCenter(rect: Rect): Point {
+  return {
+    x: (rect.xMin + rect.xMax) / 2,
+    y: (rect.yMin + rect.yMax) / 2,
+  };
 }
 
-export function rectSize(rect) {
+export function rectSize(rect: Rect): Size {
   return {
     width: Math.abs(rect.xMax - rect.xMin),
     height: Math.abs(rect.yMax - rect.yMin),
   };
 }
 
-export function rectFromArray(array) {
+export function rectFromArray(array: [number, number, number, number]): Rect {
   if (array.length != 4) {
     throw new Error("rect array must have length == 4");
   }
-  return { xMin: array[0], yMin: array[1], xMax: array[2], yMax: array[3] };
+  return {
+    xMin: array[0],
+    yMin: array[1],
+    xMax: array[2],
+    yMax: array[3],
+  };
 }
 
-export function rectToArray(rect) {
+export function rectToArray(rect: Rect): [number, number, number, number] {
   return [rect.xMin, rect.yMin, rect.xMax, rect.yMax];
 }
 
-export function isEmptyRect(rect) {
+export function isEmptyRect(rect: Rect) {
   const size = rectSize(rect);
   return size.width === 0 && size.height === 0;
 }
 
-export function rectFromPoints(points) {
+export function rectFromPoints(points: Point[]): Rect | undefined {
   if (!points.length) {
     return undefined;
   }
@@ -155,7 +178,7 @@ export function rectFromPoints(points) {
   return { xMin, yMin, xMax, yMax };
 }
 
-export function rectToPoints(rect) {
+export function rectToPoints(rect: Rect): [Point, Point, Point, Point] {
   return [
     { x: rect.xMin, y: rect.yMin },
     { x: rect.xMax, y: rect.yMin },
@@ -164,7 +187,7 @@ export function rectToPoints(rect) {
   ];
 }
 
-export function updateRect(rect, point) {
+export function updateRect(rect: Rect, point: Point): Rect {
   // Return the smallest rect that includes the original rect and the given point
   return {
     xMin: Math.min(rect.xMin, point.x),
@@ -174,7 +197,7 @@ export function updateRect(rect, point) {
   };
 }
 
-export function rectAddMargin(rect, relativeMargin) {
+export function rectAddMargin(rect: Rect, relativeMargin: number) {
   const size = rectSize(rect);
   const inset =
     size.width > size.height
@@ -183,14 +206,14 @@ export function rectAddMargin(rect, relativeMargin) {
   return insetRect(rect, -inset, -inset);
 }
 
-export function rectScaleAroundCenter(rect, scaleFactor, center) {
+export function rectScaleAroundCenter(rect: Rect, scaleFactor: number, center: Point) {
   rect = offsetRect(rect, -center.x, -center.y);
   rect = scaleRect(rect, scaleFactor);
   rect = offsetRect(rect, center.x, center.y);
   return rect;
 }
 
-export function rectRound(rect) {
+export function rectRound(rect: Rect): Rect {
   return {
     xMin: Math.round(rect.xMin),
     yMin: Math.round(rect.yMin),
@@ -199,7 +222,7 @@ export function rectRound(rect) {
   };
 }
 
-export function validateRect(rect) {
+export function validateRect(rect: Rect) {
   if (
     !rect ||
     !isNumber(rect.xMin) ||
